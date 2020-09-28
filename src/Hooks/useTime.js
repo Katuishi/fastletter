798c6fdc0e30e75b5d893a6d703daf3a42d5f267
Jaden <diogenes_mj@hotmail.com>
2020-09-27 20:51:30 -0400

    hooks for game

---
+++ src/Hooks/useTime.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+export default function (seconds) {
+  const [time, setTime] = useState(seconds);
+  const [timeover, setTimeOver] = useState(false);
+
+  const resetTime = () => {
+    setTimeOver(false);
+    setTime(seconds);
+  };
+  useEffect(() => {
+    const watch = setTimeout(() => {
+      setTime(time - 1);
+    }, 1000);
+
+    if (!time) {
+      console.log("stop");
+      setTimeOver(true);
+      clearTimeout(watch);
+    }
+    return () => {
+      clearTimeout(watch);
+    };
+  }, [time]);
+
+  return {
+    time,
+    timeover,
+    resetTime,
+  };
+}
