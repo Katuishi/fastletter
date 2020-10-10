5e4a2c444e99d08b94cee8accbc3438d6e6e9871
Jaden <diogenes_mj@hotmail.com>
2020-10-10 19:42:47 -0400

    delete location

--- src/Components/Dashboard/index.js
+++ src/Components/Dashboard/index.js
@@ -1,21 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Style from "./Style.module.css";
-import { useLocation } from "react-router-dom";
-
-
 export default function Dashboard() {
-  const { location } = useLocation()
-  
   return (
     <div className={Style.container}>
-      <section className={Style.box} >
+      <section className={Style.box}>
         <h1>FastLetter</h1>
         <Link to="/level">
           <button className={Style.button}>Start Game</button>
         </Link>
         <div className={Style.spacebar}></div>
-        <Link  to="/scorebank">
+        <Link to="/scorebank">
           <button className={Style.button}>Scorebank</button>
         </Link>
       </section>
--- src/Views/Game.js
+++ src/Views/Game.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { Game } from "../Components";
-import { useLocation } from "react-router-dom";
 
 export default function GameLayout() {
-  const { state } = useLocation();
-
   return (
     <>
-      <Game level={state.level?state.level:3}></Game>
+      <Game></Game>
     </>
   );
 }
